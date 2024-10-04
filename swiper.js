6db7242cc692c1059b84807e40cd6325e991af3b
Byorks <vanessabyorksp@gmail.com>
2024-10-04 11:45:28 -0300

    :sparkles: feat:Banner e Categorias Responsivas

--- swiper.js
+++ swiper.js
@@ -1,4 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".SwiperCategoryMobile", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper2 = new Swiper(".SwiperCategoryDesktop", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper3 = new Swiper(".SwiperBanner", {
     spaceBetween: 30,
     pagination: {
         el: ".swiper-pagination",
